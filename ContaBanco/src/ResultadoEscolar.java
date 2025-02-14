0350366696906ed0e02ce441cca1f5ddb1f5aa05
aspli <alexandre.xoquito@gmail.com>
2025-02-14 15:44:42 -0300

    [refact] Condicional Ternária

--- ContaBanco/src/ResultadoEscolar.java
+++ ContaBanco/src/ResultadoEscolar.java
@@ -4,11 +4,24 @@ public class ResultadoEscolar {
         
         int nota = 6;
 
-        if(nota >= 7)
-            System.out.println("Aprovado!");
-        else if(nota >= 5 && nota < 7)
-            System.out.println("Recuperação!");
-        else
-            System.out.println("Reprovado!");
+        // REFACTOR
+        //String resultado = nota >=7 ? "Aprovado!" : "Reprovado!"; 
+
+        // REFACTOR IF-ESLE IF-ELSE
+        String resultadoFinal = nota >=7 ? "Aprovado!" : nota >=5 && nota < 7 ? "Recuperação!" : "Reprovado!";
+
+        System.out.println(resultadoFinal);
+
+        // if (nota >= 7)
+        //     System.out.println("Aprovado!");
+        // else if (nota >= 5 && nota < 7)
+        //     System.out.println("Recuperação!");
+        // else
+        //     System.out.println("Reprovado!");
+        
+        
+
+
+
     }
 }
