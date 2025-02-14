45a7fdfc10fe7e4488df204b52d29d561bc1f629
aspli <alexandre.xoquito@gmail.com>
2025-02-14 15:24:30 -0300

    [feat] Add Controle de fluxo composto

--- ContaBanco/src/CaixaEletronico.java
+++ ContaBanco/src/CaixaEletronico.java
@@ -1,12 +1,14 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 28.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
-        
-        System.out.println(saldo);
+            System.out.println(saldo);
+        }else
+            System.out.println("Impossível realizar saque! \n << Valor indisponível >>");
+            
     }
     
 }
